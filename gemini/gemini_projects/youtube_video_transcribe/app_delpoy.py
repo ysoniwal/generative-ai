5c82f820115e39195e5d3f0f8d9c2c90481f8d43
Yogesh Soniwal <yogeshsoniwal@gmail.com>
2024-04-20 21:55:32 +0530

    Correcting UI of youtube summarizer app

--- gemini/gemini_projects/youtube_video_transcribe/app_delpoy.py
+++ gemini/gemini_projects/youtube_video_transcribe/app_delpoy.py
@@ -9,14 +9,14 @@ chunk_overlap = 100
 
 import streamlit as st
 
+from utils import display_video_thumbnail
 from utils import extract_youtube_transcript_detail
 from utils import get_pinecone_index
+from utils import preprocess_transcipt
 from utils import get_query_response
 
-import google.generativeai as genai
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+import google.generativeai as genai
 
 # Configure Generative AI with Google API KEY
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -24,96 +24,44 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 # Load google pro 1.5 model endpoint
 llm_model = genai.GenerativeModel('models/gemini-1.5-pro-latest')
 
-# # Load gemini embeddings
-# embeddings_model = genai.GenerativeModel('models/embedding-001')
-embeddings_model = OpenAIEmbeddings(api_key=os.environ["OPENAI_API_KEY"])
-
-def user_question_response(youtube_link, user_question):
-    # Step 1. From the input YouTube url, transcribe to text
-    # Frontend part
-
-    
-
-            user_query = st.text_input("Ask anything about the video", key='user_query')
-            if st.button("Enter") and user_query:
-
-                # Step 2. Create chunks from text for converting to chunks
-                ts = RecursiveCharacterTextSplitter(chunk_size=max_chars, chunk_overlap=chunk_overlap)
-                chunks = ts.split_text(transcript_text)
-
-                # Step 3. Create embeddings using OpenAIEmbeddings model
-                # This model has default embedding size (1536) and doesn't support changing it
-                embeddings_model = OpenAIEmbeddings(api_key=os.environ["OPENAI_API_KEY"])
-
-                # Step 4. Store these embeddings in vector databases. Index name we have created is youtubetranscribevector
-                index = get_pinecone_index("youtubetranscribevector", chunks, embeddings_model)
-
-                # Step 5. For the specified query, search similar results
-                #user_query = "What is unemployment rate in Indian currently"
-                user_query = user_question
-                maching_documents=index.similarity_search(user_query, k=5)
-
-                # Step 6. Answer user query from the document
-                response = get_query_response(llm_model, user_query, maching_documents)
-
-                st.write(response)
-
 def main():
-    st.set_page_config(page_title="Ask questions from YouTube video")
+    st.set_page_config(page_title="YouTube video Q&A App")
     
-    transcript_text = ""
-    video_summary = ""
-    youtube_link = ""
-    selected_option = None
+    st.title("YouTube video Q&A App")
+    youtube_link = st.text_input("Enter Youtube video link", key='input_video')
+
+    # This is query entered by user. Initializing with empty
+    user_query = st.empty()
+    # Whether pre processing is complete or not
+    processing_complete = False
+
+    button1 = st.button('Submit')
+    # In streamlit state of a button is True only when it is clicked
+    # As soon as any other action is performed on the page, the state
+    # again becomes False. Here we are storing state of the first button
+    if st.session_state.get('button') != True:
+        st.session_state['button'] = button1
+
+    if st.session_state['button'] and youtube_link:
+        pinecone_index = preprocess_transcipt(youtube_link, llm_model, max_chars, chunk_overlap)
+        processing_complete = True
     
-    # The first step before question answering is Uploading the PDF and creating embeddings
-    with st.sidebar:
-        st.title("YouTube video Q&A App")
-        youtube_link = st.text_input("Enter Youtube video link", key='input_video')
+        user_query = ""
+        if processing_complete:
+            #if vector_store:
+            user_query = st.text_input("Ask anything about the video", key='user_query')
+            #st.write(user_query)
 
-        if st.button("Enter") and youtube_link:
-            # Show thumbnail
-            video_id = youtube_link.split("=")[1]
-            image_link = f"http://img.youtube.com/vi/{video_id}/0.jpg"
-            st.image(image_link, use_column_width=True)
-            st.text("URL fetch successfull. Click on Get Video Summary to proceed..")
-            st.success("Done", icon="✅")
+        if st.button("Ask") and user_query:
 
-            selected_option = st.selectbox("Choose an option:", ["Video Summary", "Question from video"])
+            # Step 5. For the specified query, search similar results
+            #user_query = "What is unemployment rate in Indian currently"
+            matching_documents = pinecone_index.similarity_search(user_query, k=5)
 
-    if selected_option:
-        #if st.button("Get Video Summary"):
-            transcript_text = extract_youtube_transcript_detail(youtube_link)
-            prompt_summary = """
-                You are a youtube video summarizer. You will be taking a youtube video transcript.
-                Your job is to summarize the transcript. Your output should be following:
-                1. The title or topic of the video 
-                2. Short summary (less than 50 words)
-                3. Long summary (within 300 words)
-                4. What is the percentage of relevant content. For this you have to cosider the part of summary where actual topic of the video is being discussed. 
-                For eg, you can ignore parts where the video promotes some related product or request for like, share, susbscribe etc., or tells about related
-                videos or content. 
-                All of the above have be done in both English and Hindi
-                Here is the trasnsript text:
-                """
-                
-            if transcript_text is not None:
-                video_summary=llm_model.generate_content(prompt_summary + transcript_text)
-                video_summary=video_summary.text
-                #st.subheader("Video summary is")
-                #st.write(response.text)
+            # Step 6. Answer user query from the document
+            response = get_query_response(llm_model, user_query, matching_documents)
 
-            if selected_option == "Video Summary":
-                st.write(video_summary)
-            elif selected_option == "Question from video":
-                st.write("Test")
-    # print(youtube_link)
-    # # If the user asks question, then get the response
-    # st.header("Ask a question after uploading YouTube video URL")
-    # user_question = st.text_input("Ask a Question from video")
-    # if user_question:
-    #     user_question_response(youtube_link, user_question)
-    #     pass
-    
+            st.write(response)
+        
 if __name__ == "__main__":
     main()
--- gemini/gemini_projects/youtube_video_transcribe/utils.py
+++ gemini/gemini_projects/youtube_video_transcribe/utils.py
@@ -2,6 +2,16 @@ import os
 from youtube_transcript_api import YouTubeTranscriptApi
 from pinecone import Pinecone
 from langchain_community.vectorstores import Pinecone as PC
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+
+import streamlit as st
+
+def display_video_thumbnail(youtube_video_url):
+    video_id = youtube_video_url.split("=")[1]
+    image_link = f"http://img.youtube.com/vi/{video_id}/0.jpg"
+    st.image(image_link, use_column_width=True)
+    st.write("Transcript Fetched. Generating Summary...")
 
 def extract_youtube_transcript_detail(youtube_video_url):
     """
@@ -34,8 +44,24 @@ def extract_youtube_transcript_detail(youtube_video_url):
     
     except Exception as e:
         raise e
- 
-def get_pinecone_index(index_name, chunks, embeddings_model):
+
+def get_pinecone_index(transcript_text, max_chars, chunk_overlap):
+    # Vector Store
+    # Create chunks from text for converting to embeddings
+    ts = RecursiveCharacterTextSplitter(chunk_size=max_chars, chunk_overlap=chunk_overlap)
+    chunks = ts.split_text(transcript_text)
+
+    # Create embeddings using OpenAIEmbeddings model
+    # This model has default embedding size (1536) and doesn't support changing it
+    embeddings_model = OpenAIEmbeddings(api_key=os.environ["OPENAI_API_KEY"])
+
+    # Store these embeddings in vector databases. Index name we have created is youtubetranscribevector
+    pinecone_index = upsert_to_pinecone_index("youtubetranscribevector", chunks, embeddings_model)
+
+    return pinecone_index
+
+
+def upsert_to_pinecone_index(index_name, chunks, embeddings_model):
     pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
     index = pc.Index(index_name)
     # Delete all existing vectors in the index
@@ -48,6 +74,43 @@ def get_pinecone_index(index_name, chunks, embeddings_model):
     index=PC.from_texts(chunks, embeddings_model, index_name=index_name)
     return index
 
+def preprocess_transcipt(youtube_link, llm_model, max_chars, chunk_overlap):
+    pinecone_index = None
+
+    # Display thumbnail
+    display_video_thumbnail(youtube_link)
+
+    # Extract transctipt of the youtube video
+    transcript_text = extract_youtube_transcript_detail(youtube_link)
+    
+    # Prompt for generating summary
+    prompt_summary = """
+        You are a youtube video summarizer. You will be taking a youtube video transcript.
+        Your job is to summarize the transcript. Your output should be following:
+        1. The title or topic of the video 
+        2. Short summary (less than 50 words)
+        3. Long summary (within 300 words)
+        4. What is the percentage of relevant content. For this you have to cosider the part of summary where actual topic of the video is being discussed. 
+        For eg, you can ignore parts where the video promotes some related product or request for like, share, susbscribe etc., or tells about related
+        videos or content. 
+        You have to give response in same language as that of the primary language of this video.
+        Here is the trasnsript text:
+        """
+        
+    if transcript_text is not None:
+        # Display Video Summary
+        video_summary=llm_model.generate_content(prompt_summary + transcript_text)
+        video_summary=video_summary.text
+        st.subheader("Video summary is")
+        st.write(video_summary)
+
+        pinecone_index = get_pinecone_index(transcript_text, max_chars, chunk_overlap)
+        
+        # st.write("Vector Store created !!!")
+        st.success("Summary Generated!!", icon="✅")
+
+        return pinecone_index
+    
 def get_query_response(llm_model, user_query, maching_documents):
     prompt = """
     You will be given some context and you will be asked a question. You have to provide answer from the specified context only.
@@ -57,5 +120,3 @@ def get_query_response(llm_model, user_query, maching_documents):
 
     response=llm_model.generate_content(prompt + user_query + str(maching_documents))
     return response.text
-
-    
